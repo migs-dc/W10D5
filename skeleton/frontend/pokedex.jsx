b3ccefddfce7061f2a97eb97efe945d731f457b9
Priyesh Shah <priyesh.shah147@gmail.com>
2021-06-25 16:12:47 -0400

    phase1

---
+++ skeleton/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, rootEl);
+});
