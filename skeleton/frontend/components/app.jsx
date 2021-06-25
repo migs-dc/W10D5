fff5a5361028ed920a7c5a7d7c10196bd1264bff
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-06-25 17:00:04 -0400

    phase 2

---
+++ skeleton/frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div className="app">
+    <h1>Super Awesome Pokedex</h1>
+    <TodoListContainer />
+  </div>
+);
+
+export default App;
--- skeleton/frontend/pokedex.jsx
+++ skeleton/frontend/pokedex.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root'
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, rootEl);
+  // ReactDOM.render(<h1>hello world</h1>, rootEl);
 });
